0fe12eace4046482295883692de946ff6957b000
Shahin Mahmud <rsuitsit@gmail.com>
2018-06-01 10:53:24 +0600

    Create guessing_game.py

---
+++ guessing_game.py
@@ -0,0 +1,5 @@
+# Guessing game
+# write a program where your program generate a random number, and the player have to guess it within 5 terms
+# if he correctly guess it within 5 times, he wins or he fails
+
+
