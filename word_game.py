e2acb881ea9a7cc3cc07ed13a425dafb0fb0ab57
Shahin Mahmud <rsuitsit@gmail.com>
2018-06-01 10:40:26 +0600

    Create word_game.py

---
+++ word_game.py
@@ -0,0 +1,2 @@
+# Problem: Create a word game where player one inputs a valid word, and player two have to input a valid word containing the last letter
+# of the player one's input as the first letter, the game continues untill one of the player fails to input a correct word within 15 seconds
